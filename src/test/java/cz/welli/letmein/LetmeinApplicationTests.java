369ae139e33f9336f862dc26d20a0e938f1afead
Michael Kinc <kinc.michael@gmail.com>
2022-11-08 17:15:47 +0100

    Initial commit

---
+++ src/test/java/cz/welli/letmein/LetmeinApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.welli.letmein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetmeinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
