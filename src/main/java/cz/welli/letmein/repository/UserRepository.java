6fcb6d02521588459f72488db9bd0b4c149514e3
Michael Kinc <kinc.michael@gmail.com>
2022-12-13 19:37:40 +0100

    Implement part of registration and login process

---
+++ src/main/java/cz/welli/letmein/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package cz.welli.letmein.repository;
+
+import cz.welli.letmein.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    public User findByEmail(String email);
+}
---
+++ src/main/java/cz/welli/letmein/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package cz.welli.letmein.service;
+
+import cz.welli.letmein.controller.CustomUserDetails;
+import cz.welli.letmein.entity.User;
+import cz.welli.letmein.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("userService")
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if(user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new CustomUserDetails(user);
+    }
+}
