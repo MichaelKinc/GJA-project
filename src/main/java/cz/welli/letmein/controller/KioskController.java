8819b5a1cb1461e9f457076352c38a1571b2c96c
Petr Vrtal <xvrtal01@stud.fit.vutbr.cz>
2023-01-23 17:53:32 +0100

    Simple verification

--- src/main/java/cz/welli/letmein/controller/KioskController.java
+++ src/main/java/cz/welli/letmein/controller/KioskController.java
@@ -2,16 +2,32 @@ package cz.welli.letmein.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import cz.welli.letmein.entity.PinCodeFormResponse;
 
 @Controller
 public class KioskController {
     @GetMapping("/kiosk")
     String getKiosk (Model model) {
         model.addAttribute("uploaded", false);
-        model.addAttribute("verification_result", "fail");
         return "kiosk";
     }
 
-
+    @PostMapping("/kiosk")
+    String verifyKioskCode (PinCodeFormResponse pin, Model model) {
+        if (pin.isValid()) {
+            //Validation
+            if (pin.comparePin("111111")) {
+                model.addAttribute("verification_result", "success");
+            } else {
+                model.addAttribute("verification_result", "fail");
+            }
+        }
+        model.addAttribute("uploaded", true);
+        return "kiosk";
+    }
 }
---
+++ src/main/java/cz/welli/letmein/entity/PinCodeFormResponse.java
@@ -0,0 +1,24 @@
+package cz.welli.letmein.entity;
+
+public class PinCodeFormResponse {
+    private String pin;
+
+    public PinCodeFormResponse(String pin) {
+        this.pin = pin;
+    }
+
+    public String getPin() {
+        return this.pin;
+    }
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public boolean comparePin(String pin) {
+        return this.pin.equals(pin);
+    }
+
+    public boolean isValid() {
+        return (this.pin.length() == 6);
+    }
+}
