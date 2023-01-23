c36b0a8b756660867572bc0bf47a2eae3cedb5cc
Marek Stastny <marek.stny@gmail.com>
2023-01-23 23:43:43 +0100

    Fix bug with unrecheable admin space

--- src/main/java/cz/welli/letmein/controllers/Admin.java
+++ src/main/java/cz/welli/letmein/controllers/Admin.java
@@ -1,10 +1,12 @@
 package cz.welli.letmein.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class Admin {
     @GetMapping("/home/admin")
-    public String setupHome() {
+    public String setuAdmin() {
         return "admin";
     }
 
