0378d81d79ae202931eaf0981b198fa9f036e51d
Marek Stastny <marek.stny@gmail.com>
2023-01-24 16:24:18 +0100

    feat: send email when route to /home

--- src/main/java/cz/welli/letmein/controllers/Home.java
+++ src/main/java/cz/welli/letmein/controllers/Home.java
@@ -1,13 +1,22 @@
 package cz.welli.letmein.controllers;
 
+import cz.welli.letmein.services.EmailServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Home {
 
+    @Autowired
+    EmailServiceImpl es;
     @GetMapping("/home")
     public String setupHome() {
+
+        es.sendSimpleMessage("kmk@sdsds.cs", "test", "test message");
+
+
+
 
         return "home";
     }
