2559c16dae1e4e59523bfd9f6877339522c47f13
Marek Stastny <marek.stny@gmail.com>
2023-01-26 20:36:40 +0100

    Add some comments and delete unused methods

--- src/main/java/cz/welli/letmein/config/CustomAuthenticationSuccessHandler.java
+++ src/main/java/cz/welli/letmein/config/CustomAuthenticationSuccessHandler.java
@@ -12,13 +12,22 @@ import java.io.IOException;
 import java.util.Set;
 
 /**
- * Method of this class is responsible for routing after login
+ * Method of this class is responsible for redirect after login
  */
 @Configuration
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
+
+    /**
+     * User is redirected to /kiosk when has role ROLE_KIOSK and to /home otherwise
+     *
+     * @param httpServletRequest httpServletRequest
+     * @param httpServletResponse httpServletResponse
+     * @param authentication authentication
+     * @throws IOException If an input or output exception occurs
+     */
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException {
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
         if (roles.contains("ROLE_KIOSK")) {
             httpServletResponse.sendRedirect("/kiosk");
