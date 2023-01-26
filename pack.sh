62fe419018e71e143f83e9499c0f6b3f75904f89
Marek Stastny <marek.stny@gmail.com>
2023-01-26 23:48:38 +0100

    done

---
+++ pack.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#rm -r ./src/main/resources/static
+rm xkincm02_xstast33_xvrtal01.zip
+zip -r xkincm02_xstast33_xvrtal01 src pom.xml README.md
--- src/main/java/cz/welli/letmein/controllers/KioskController.java
+++ src/main/java/cz/welli/letmein/controllers/KioskController.java
@@ -36,9 +36,10 @@ public class KioskController {
 
     /**
      * Verify pin code method
-     * @param pin
-     * @param model
-     * @return
+     *
+     * @param pin pin given by user
+     * @param model model
+     * @return kiosk view
      */
     @PostMapping("/kiosk")
     String verifyKioskCode (PinCodeFormResponse pin, Model model) {
